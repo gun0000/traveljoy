784f2274bcf89a8097bcbe547a1601857b414d61
guncode4545 <guncode4545@gmail.com>
2023-11-20 13:44:44 +0900

    초기세팅-QueryDsl세팅

---
+++ src/main/java/com/traveljoy/TraveljoyApplication.java
@@ -0,0 +1,13 @@
+package com.traveljoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveljoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveljoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traveljoy/TraveljoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveljoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveljoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
