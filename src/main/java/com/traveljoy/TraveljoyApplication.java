f89d4df165746a0602d3757ed00084b406e6dd26
guncode4545 <guncode4545@gmail.com>
2023-11-27 22:36:15 +0900

    BaseTimeEntity위한 설정추가

--- src/main/java/com/traveljoy/TraveljoyApplication.java
+++ src/main/java/com/traveljoy/TraveljoyApplication.java
@@ -2,8 +2,10 @@ package com.traveljoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TraveljoyApplication {
 
 	public static void main(String[] args) {
